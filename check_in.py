6fd26009126c8ecb85617e077b47a48bc34a07bf
Brian Jones <joshaidan@gmail.com>
2017-05-24 22:10:47 -0400

    Display the table and multiple names

--- check_in.py
+++ check_in.py
@@ -19,25 +19,53 @@ class CheckIn:
         """Processing a check in"""
         self.screen.fill((0, 0, 0))
 
-        text = self.font.render('Please wait...', False, (255, 255, 255))
+        text = self.font.render('Please wait...', True, (255, 255, 255))
         self.screen.blit(text, (int(self.screen.get_width() / 2), int(self.screen.get_height() / 2)))
 
         # Look up user_id in database
-        c.execute('SELECT * FROM people WHERE uuid = ?', (user_id,))
-        person = c.fetchone()
-        if not person:
+        c.execute('SELECT * FROM cards INNER JOIN people ON cards.group_id = people.group_id WHERE uuid = ?', (user_id,))
+        
+        if c.rowcount == 0:
             self.screen.fill((0, 0, 0))
-            text = self.font.render("Sorry, we couldn't find you in the database. Please ask for help.", False, (255, 255, 255))
+            text = self.font.render("Sorry, we couldn't find you in the database. Please ask for help.", True, (255, 255, 255))
             self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), int(self.screen.get_height() / 2)))
-            text = self.font.render(user_id, False, (255, 255, 255))
+            text = self.font.render(user_id, True, (255, 255, 255))
             self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), int(self.screen.get_height() / 2) + text.get_height()))
         else:
             self.screen.fill((0, 0, 0))
-            text = self.font.render("Hi %s!" % person['firstname'], False, (255, 255, 255))
-            self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), int(self.screen.get_height() / 2)))
+            rectangle = pygame.draw.rect(self.screen, (0, 0, 255), (50, 50, self.screen.get_width() - 100, self.screen.get_height() - 100), 10)
+
+            text = self.font.render("June 10, 2017", True, (255, 255, 255))
+            self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), 3))
+
+            text = self.font.render("Welcome to", True, (255, 255, 255))
+            self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), text.get_height() * 4))
+
+            text = self.font.render("Brian & Aleisha's Wedding", True, (255, 255, 255))
+            self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), (text.get_height() + 3) * 5))
+
+            text = self.font.render("Honoured guests...", True, (255, 255, 255))
+            self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), (text.get_height() + 3) * 8 + 10))
+
+            i = 0
+            person = c.fetchone()
+            table_id = person['table_id']
+            while person:
+                text = self.font.render("%s %s" % (person['firstname'], person['lastname']), True, (255, 255, 255))
+                self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), (text.get_height() + 3) * i + 350))
+                i += 1
+                person = c.fetchone()
+
+            c.execute("SELECT * FROM tables WHERE id = %s" % table_id)
+            table = c.fetchone()
+            if table:
+                table_name = table['name']
+
+                text = self.font.render("Your table is %s" % table_name, True, (255, 255, 255))
+                self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), (text.get_height() + 3) * 12 + 10))
 
         pygame.display.flip()
-        time.sleep(5)
+        time.sleep(10)
 
     def watch_check_in(self):
         # Scan for a card
--- database.sql
+++ database.sql
@@ -17,7 +17,7 @@ CREATE TABLE people (
 CREATE TABLE cards (
     uuid text,
     group_id int
-)
+);
 
 CREATE TABLE tables (
     id int,
@@ -26,4 +26,8 @@ CREATE TABLE tables (
 );
 
 INSERT INTO people (id, firstname, lastname, group_id, table_id) VALUES (1, 'Brian', 'Jones', 1, 1);
+INSERT INTO people (id, firstname, lastname, group_id, table_id) VALUES (2, 'Aleisha', 'Jones', 1, 1);
+
+INSERT INTO tables (id, name, number) VALUES (1, 'Head Table', 1);
+
 INSERT INTO cards (uuid, group_id) VALUES ('144-248-110-133', 1);
