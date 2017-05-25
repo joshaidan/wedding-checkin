0d28750071be512606c2b4fa88d77cff58d7b47e
Brian Jones <joshaidan@gmail.com>
2017-05-24 20:21:43 -0400

    Clean up linting

--- check_in.py
+++ check_in.py
@@ -17,20 +17,20 @@ class CheckIn:
 
     def check_in(self, user_id):
         """Processing a check in"""
-        self.screen.fill((0,0,0))
+        self.screen.fill((0, 0, 0))
 
-        text = self.font.render('Please wait...', False, (255,255,255))
+        text = self.font.render('Please wait...', False, (255, 255, 255))
         self.screen.blit(text, (int(self.screen.get_width() / 2), int(self.screen.get_height() / 2)))
 
         # Look up user_id in database
         c.execute('SELECT * FROM people WHERE uuid = ?', (user_id,))
         person = c.fetchone()
         if not person:
-            self.screen.fill((0,0,0))
-            text = self.font.render("Sorry, we couldn't find you in the database. Please ask for help.", False, (255,255,255))
+            self.screen.fill((0, 0, 0))
+            text = self.font.render("Sorry, we couldn't find you in the database. Please ask for help.", False, (255, 255, 255))
             self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), int(self.screen.get_height() / 2)))
         else:
-            self.screen.fill((0,0,0))
+            self.screen.fill((0, 0, 0))
             text = self.font.render("Hi %s!" % person['firstname'], False, (255,255,255))
             self.screen.blit(text, (int(self.screen.get_width() / 2 - (text.get_width() / 2)), int(self.screen.get_height() / 2)))
 
@@ -45,5 +45,6 @@ class CheckIn:
             # Card found
             (status, uid) = self.MIFAREReader.MFRC522_Anticoll()
             if status == self.MIFAREReader.MI_OK:
-                card_id = str(uid[0]) + str(uid[1]) + str(uid[2]) + str(uid[3])
+                card_id = str(uid[0]) + '-' + str(uid[1]) + '-' + str(uid[2]) + '-' + str(uid[3])
                 self.check_in(card_id)
+                
